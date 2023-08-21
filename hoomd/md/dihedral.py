cbb32ae3b85306c6394406713b4b6cd5251647e1
judevishnu <jvishnu@uni-mainz.de>
2023-08-21 14:26:20 +0200

    update

--- hoomd/md/dihedral.py
+++ hoomd/md/dihedral.py
@@ -166,7 +166,7 @@ class Torsional(Dihedral):
 
         params = TypeParameter(
             'params', 'dihedral_types',
-            TypeParameterDict(k=float, d=float, n=int, phi0=float, torque=(float,float,float), len_keys=1))
+            TypeParameterDict(k=float, d=float, n=int, phi0=float, torque=(0.,0.,0.), len_keys=1))
         self._add_typeparam(params)
 
     def _attach(self):
