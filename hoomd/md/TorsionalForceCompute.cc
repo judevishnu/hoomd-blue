80aa357a249bda703e79ecf56dba9f163d7726fc
judevishnu <jvishnu@uni-mainz.de>
2023-08-23 00:42:08 +0200

    newup

--- hoomd/md/TorsionalForceCompute.cc
+++ hoomd/md/TorsionalForceCompute.cc
@@ -322,7 +322,7 @@ void TorsionalForceCompute::computeForces(uint64_t timestep)
         h_torque.data[rtagn].x += torqn.x;
         h_torque.data[rtagn].y += torqn.y;
         h_torque.data[rtagn].z += torqn.z;
-        h_torque.data[rtagn].w = 0
+        h_torque.data[rtagn].w = 0;
 
         //printf("%f %f %f %f %f %f %f %f\n",angl,h_torque.data[rtagp].z,h_torque.data[rtagn].z,torqp.z,torqn.z,m_K[dihedral_type],ss,cs);
         //
